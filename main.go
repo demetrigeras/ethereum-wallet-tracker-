1761fa0fc21420ea8fdf12bdf637123603f4d336
demetrigeras <demetrijgeras@gmail.com>
2023-11-29 10:28:44 -0500

    test

--- main.go
+++ main.go
@@ -1,25 +1,62 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
-
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
+	"net/http"
 )
 
+// EtherscanResponse struct to hold the JSON response from Etherscan API
+type EtherscanResponse struct {
+	Status  string        `json:"status"`
+	Message string        `json:"message"`
+	Result  []Transaction `json:"result"`
+}
+
+// Transaction struct to hold individual transaction details
+type Transaction struct {
+	Hash  string `json:"hash"`
+	From  string `json:"from"`
+	To    string `json:"to"`
+	Value string `json:"value"`
+	// Include other fields as required, like From, To, Value, etc.
+}
+
 func main() {
-	client, err := ethclient.Dial("https://mainnet.infura.io/v3/YOUR_INFURA_API_KEY")
+	// Replace with your wallet address and Etherscan API key
+	walletAddress := "0xdE0336765d7549fB555883eB6c85e8862b4fDc41"
+	apiKey := "J48JTZN2IZVYUUA7DME5IVPBKMM4YAMF68"
+
+	// Constructing the Etherscan API URL
+	url := fmt.Sprintf("https://api.etherscan.io/api?module=account&action=txlist&address=%s&startblock=0&endblock=99999999&sort=asc&apikey=%s", walletAddress, apiKey)
+
+	// Making the HTTP GET request to the Etherscan API
+	resp, err := http.Get(url)
 	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+		log.Fatalf("Error fetching transactions: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Reading the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Error reading response: %v", err)
 	}
 
-	address := common.HexToAddress("YOUR_ETH_ADDRESS")
-	balance, err := client.BalanceAt(context.Background(), address, nil)
-	if err != nil {
-		log.Fatalf("Failed to get the balance for the address: %v", err)
+	// Unmarshalling the JSON response into the EtherscanResponse struct
+	var response EtherscanResponse
+	if err := json.Unmarshal(body, &response); err != nil {
+		log.Fatalf("Error parsing JSON: %v", err)
 	}
 
-	fmt.Printf("Balance for address %s: %s ETH\n", address.Hex(), balance)
+	// Iterating over and printing each transaction
+	for _, tx := range response.Result {
+		fmt.Printf("Transaction Hash: %s\n", tx.Hash)
+		fmt.Printf("Transaction From: %s\n", tx.From)
+		fmt.Printf("Transaction To: %s\n", tx.To)
+		fmt.Printf("Transaction Value: %s\n", tx.Value)
+		// Print additional transaction details as needed
+	}
 }
